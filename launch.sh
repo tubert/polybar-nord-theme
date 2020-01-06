e33176763ce58fc5cd794f6d7481e76f80ed618c
Sergi Tubert Sabater <stubertsabater@gmail.com>
2020-01-06 22:16:48 +0100

    Polybar launches in every monitor available. DPI set to 0.

--- launch.sh
+++ launch.sh
@@ -6,14 +6,30 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-if [ "$1" == "light" ]
-then
-	polybar -c $HOME/.config/polybar/light-config nord-top &
-	polybar -c $HOME/.config/polybar/light-config nord-down &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	  # Launch bar1 and bar2
+	if [ "$1" == "light" ]
+	then
+		MONITOR=$m polybar -c $HOME/.config/polybar/light-config nord-top &
+		MONITOR=$m polybar -c $HOME/.config/polybar/light-config nord-down &
+	else
+		MONITOR=$m polybar -c $HOME/.config/polybar/dark-config nord-top &
+		MONITOR=$m polybar -c $HOME/.config/polybar/dark-config nord-down &
+	fi
+  done
 else
-	polybar -c $HOME/.config/polybar/dark-config nord-top &
-	polybar -c $HOME/.config/polybar/dark-config nord-down &
+	  # Launch bar1 and bar2
+	if [ "$1" == "light" ]
+	then
+		polybar -c $HOME/.config/polybar/light-config nord-top &
+		polybar -c $HOME/.config/polybar/light-config nord-down &
+	else
+		polybar -c $HOME/.config/polybar/dark-config nord-top &
+		polybar -c $HOME/.config/polybar/dark-config nord-down &
+	fi
 fi
+
+
 
 echo "Bars launched..."
